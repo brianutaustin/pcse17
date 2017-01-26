eb5cce3305b8f337a5dd9c0c4993b2b6baa091aa
Dung Phan <dphan@login2.ls5.tacc.utexas.edu>
2017-01-25 20:50:55 -0600

    Initiate hw0.cpp

---
+++ hw0/hw0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int main () {
+
+	// Smoothing coeffs
+	const float a = 0.05;
+	const float b = 0.1;
+	const float c = 0.4;
+
+	// Smoothing threshold
+	float t = 0.1;
+
+	// Number of array dimension in each direction
+	unsigned int n = 16384 + 2;
+
+	
+	return 0;
+}
+
